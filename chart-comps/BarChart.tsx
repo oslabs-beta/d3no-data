bf05cdc3bc8cb1166a8b047665b72393d7ed6bbc
swittuthcoding <swittuthcoding@gmail.com>
2022-08-03 02:39:15 -0400

    added styling to bar chart

--- chart-comps/BarChart.tsx
+++ chart-comps/BarChart.tsx
@@ -1,6 +1,5 @@
 /** @jsx h */
 import { h, Fragment, useEffect, d3 } from "../mod.ts";
-
 interface barChartData {
   width: number;
   height: number;
@@ -8,10 +7,12 @@ interface barChartData {
   label: string[];
 }
 
+// need to move all the variable up here
+
 export default function BarChart() {
   const myData: number[] = [];
   const label: string[] = [];
-  const numData = 20;
+  const numData = 12;
 
   function updateData() {
     for (let i = 0; i < numData; i++) {
@@ -37,6 +38,7 @@ export default function BarChart() {
     const barsPaddingFromYAxis = 3;
 
     // scale function for y axis
+
     const yScale = d3
       .scaleLinear()
       .domain([0, d3.max(myData)])
@@ -64,23 +66,36 @@ export default function BarChart() {
       })
       .attr("y", function (d: number): number {
         return height - yScale(d) - xAxisSize - barPaddingBottom;
-      });
+      })
+      .attr("rx", "3")
+      .attr("fill", "#BFE4A3")
+      .attr("stroke", "#90BE6D")
+      .attr("stroke-width", "2");
 
     const yAxis = d3.axisLeft(yScale).ticks(10);
     const xAxis = d3.axisBottom(xScale);
 
-    const chartContainer = d3.select(".bar-chart g");
+    const chartContainer = d3.select(".bar-chart");
 
     chartContainer
-      .append("g")
+      .insert("g", "g")
       .call(yAxis)
       // have to make this data to show for charts dynamic
-      .attr("transform", `translate(${yAxisSize}, ${chartHeightPadding})`);
+      .attr("transform", `translate(${yAxisSize}, ${chartHeightPadding})`)
+      .attr("font-size", "0.5em")
+      .attr("font-family", "Verdana")
+      .attr("color", "#4D908E")
+      .selectAll(".tick line")
+      .attr("x2", width - yAxisSize - chartWidthPadding)
+      .attr("opacity", "0.3");
 
     chartContainer
       .append("g")
       .call(xAxis)
-      .attr("transform", `translate(${yAxisSize}, ${height - xAxisSize})`);
+      .attr("transform", `translate(${yAxisSize}, ${height - xAxisSize})`)
+      .attr("font-size", "0.5em")
+      .attr("font-family", "Verdana")
+      .attr("color", "#4D908E");
   }
 
   useEffect(() => {
