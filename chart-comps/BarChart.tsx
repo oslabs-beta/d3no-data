3c8705786b7d3c4e58e01718dfeb858bf2537cff
swittuthcoding <swittuthcoding@gmail.com>
2022-08-03 09:58:54 -0400

    added styling to chart with tooltip

--- chart-comps/BarChart.tsx
+++ chart-comps/BarChart.tsx
@@ -1,5 +1,7 @@
 /** @jsx h */
 import { h, Fragment, useEffect, d3 } from "../mod.ts";
+
+// defining property for user to pass down props
 interface barChartData {
   width: number;
   height: number;
@@ -12,7 +14,7 @@ interface barChartData {
 export default function BarChart() {
   const myData: number[] = [];
   const label: string[] = [];
-  const numData = 12;
+  const numData = 7;
 
   function updateData() {
     for (let i = 0; i < numData; i++) {
@@ -42,7 +44,7 @@ export default function BarChart() {
     const yScale = d3
       .scaleLinear()
       .domain([0, d3.max(myData)])
-      .range([height - xAxisSize - chartHeightPadding, 0]);
+      .range([0, height - xAxisSize - chartHeightPadding]);
 
     // scale function for x axis
     const xScale = d3
@@ -51,6 +53,31 @@ export default function BarChart() {
       .range([0, width - yAxisSize - chartWidthPadding + barsPaddingFromYAxis]);
 
     const barWidth = (width - yAxisSize - chartWidthPadding) / numData;
+
+    const toolTip = d3
+      .select(".chart-container")
+      .append("div")
+      .style("opacity", 0)
+      .classed("tooltip", true)
+      .style("background-color", "white")
+      .style("border", "black")
+      .style("border-radius", "5px")
+      .style("padding", "5px");
+
+    function handleMouseOver(): void {
+      toolTip.style("opacity", 1);
+      d3.select(this)
+        .style("stroke", "black")
+        .style("opacity", 1)
+        .style("cursor", "pointer");
+    }
+    function handleMouseMove(d: number): void {
+      toolTip.html(`The value is ${d}`);
+    }
+    function handleMouseLeave(): void {
+      toolTip.style("opacity", 0);
+      d3.select(this).style("stroke", "none").style("opacity", 0.8);
+    }
 
     const bars = d3
       .select(".bars")
@@ -70,14 +97,20 @@ export default function BarChart() {
       .attr("rx", "3")
       .attr("fill", "#BFE4A3")
       .attr("stroke", "#90BE6D")
-      .attr("stroke-width", "2");
+      .attr("stroke-width", "2")
+      .filter(() => true)
+      .on("mouseover", handleMouseOver)
+      .on("mousemove", function (e: Event, d: number) {
+        handleMouseMove(d);
+      })
+      .on("mouseleave", handleMouseLeave);
 
     const yAxis = d3.axisLeft(yScale).ticks(10);
     const xAxis = d3.axisBottom(xScale);
 
-    const chartContainer = d3.select(".bar-chart");
+    const barChart = d3.select(".bar-chart");
 
-    chartContainer
+    barChart
       .insert("g", "g")
       .call(yAxis)
       // have to make this data to show for charts dynamic
@@ -87,15 +120,18 @@ export default function BarChart() {
       .attr("color", "#4D908E")
       .selectAll(".tick line")
       .attr("x2", width - yAxisSize - chartWidthPadding)
-      .attr("opacity", "0.3");
+      .attr("opacity", "0.3")
+      .attr("stroke-dasharray", "1, 1");
 
-    chartContainer
+    barChart
       .append("g")
       .call(xAxis)
       .attr("transform", `translate(${yAxisSize}, ${height - xAxisSize})`)
       .attr("font-size", "0.5em")
       .attr("font-family", "Verdana")
-      .attr("color", "#4D908E");
+      .attr("color", "#4D908E")
+      .selectAll(".tick line")
+      .attr("y2", "0"); // shows the ticks of the line
   }
 
   useEffect(() => {
@@ -105,12 +141,14 @@ export default function BarChart() {
 
   return (
     <Fragment>
-      <svg class="bar-chart">
-        {/* grouping for bars and axes */}
-        <g>
-          <g class="bars"></g>
-        </g>
-      </svg>
+      <div className="chart-container">
+        <svg className="bar-chart">
+          {/* grouping for bars and axes */}
+          <g>
+            <g className="bars"></g>
+          </g>
+        </svg>
+      </div>
     </Fragment>
   );
 }
