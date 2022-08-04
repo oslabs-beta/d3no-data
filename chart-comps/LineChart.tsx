7154bf010da785bb4ac8115ca9932e56936d1db3
swittuthcoding <swittuthcoding@gmail.com>
2022-08-04 17:37:30 -0400

    added tooltip circle tracker

--- chart-comps/LineChart.tsx
+++ chart-comps/LineChart.tsx
@@ -65,7 +65,7 @@ export default function LineChart() {
     svg
       .append("g")
       .call(yAxis)
-      .attr("transform", `translate(${margin.left})`)
+      .attr("transform", `translate(${margin.left}, 0)`)
       .attr("font-size", "0.5em")
       .attr("font-family", "Verdana")
       .attr("color", "#4D908E")
@@ -78,20 +78,20 @@ export default function LineChart() {
       .append("path")
       .classed("data-line", true)
       .data([data])
-      .attr("transform", `translate(${margin.left})`)
+      .attr("transform", `translate(${margin.left}, 0)`)
       .attr("fill", "none")
       .attr("stroke", "#BFE4A3")
       .attr(
         "d",
         d3
           .line()
-          .curve(d3.curveBasis)
           .x(function (d) {
             return xScale(d.x);
           })
           .y(function (d) {
             return yScale(d.y);
           })
+          .curve(d3.curveLinear)
       )
       .attr("stroke-width", 3)
       .filter(() => true)
@@ -105,67 +105,6 @@ export default function LineChart() {
       .duration(5000)
       .attr("stroke-dashoffset", 0);
 
-    const focus = d3
-      .select(".line-chart")
-      .append("g")
-      .append("circle")
-      .style("fill", "none")
-      .style("stroke", "black")
-      .style("r", 5)
-      .style("opacity", 0);
-    const focusText = d3
-      .select(".line-chart")
-      .append("g")
-      .append("text")
-      .style("opacity", 0)
-      .attr("text-anchor", "left")
-      .attr("alignment-baseline", "middle");
-    const bisect = d3.bisector(function (d) {
-      return d.y;
-    }).left;
-
-    function handleMouseOver() {
-      focus.style("opacity", 1);
-      focusText.style("opacity", 1);
-    }
-
-    function handleMouseLeave() {
-      focus.style("opacity", 0);
-      focusText.style("opacity", 0);
-    }
-
-    function handleMouseMove(e: Event) {
-      const [x, y] = d3.pointer(e);
-      const x0 = xScale.invert(x);
-      const i = bisect(data, x0, 1);
-    }
-
-    d3.select(".line-chart")
-      .on("mouseover", handleMouseOver)
-      .on("mousemove", handleMouseMove)
-      .on("mouseleave", handleMouseLeave);
-  }
-
-  // add label to the x and y axes
-  function updateLabel() {
-    d3.select(".line-chart")
-      .append("text")
-      .attr("text-anchor", "end")
-      .attr("x", margin.left + margin.right)
-      .attr("y", margin.top - 10) // between the y label and the axes
-      .attr("font-family", "Verdana")
-      .text("y label");
-
-    d3.select(".line-chart")
-      .append("text")
-      .attr("text-anchor", "middle")
-      .attr("x", (width + margin.left + margin.right) / 2)
-      .attr("y", height + margin.bottom + margin.top)
-      .attr("font-family", "Verdana")
-      .text("x label");
-  }
-
-  function updateInteractivity() {
     const focus = d3
       .select(".line-chart")
       .append("g")
@@ -198,14 +137,12 @@ export default function LineChart() {
 
     function handleMouseMove(e: Event) {
       const [x, y] = d3.pointer(e);
-      const xScale = d3.scaleTime().domain(
-        d3.extent(data, function (d: { x: Date; y: number }): Date {
-          return d.x;
-        })
-      );
-      const x0 = xScale.invert(y);
+      const x0 = xScale.invert(x);
       const i = bisect(data, x0, 1);
-      console.log(i);
+      const selectedData = data[i];
+      focus
+        .style("cx", xScale(selectedData.x) + margin.left)
+        .style("cy", yScale(selectedData.y));
     }
 
     d3.select(".line-chart")
@@ -214,13 +151,31 @@ export default function LineChart() {
       .on("mouseleave", handleMouseLeave);
   }
 
+  // add label to the x and y axes
+  function updateLabel() {
+    d3.select(".line-chart")
+      .append("text")
+      .attr("text-anchor", "end")
+      .attr("x", margin.left + margin.right)
+      .attr("y", margin.top - 10) // between the y label and the axes
+      .attr("font-family", "Verdana")
+      .text("y label");
+
+    d3.select(".line-chart")
+      .append("text")
+      .attr("text-anchor", "middle")
+      .attr("x", (width + margin.left + margin.right) / 2)
+      .attr("y", height + margin.bottom + margin.top)
+      .attr("font-family", "Verdana")
+      .text("x label");
+  }
+
   // create another function to add animation instead of grouping theme into create chart
 
   useEffect(() => {
     updateData();
     updateChart();
     updateLabel();
-    // updateInteractivity();
   }, []);
 
   return (
