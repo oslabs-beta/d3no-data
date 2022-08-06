fefe47f13c4a39fa5362aad904891d4af99eff26
swittuthcoding <swittuthcoding@gmail.com>
2022-08-06 17:49:36 -0400

    update

---
+++ chart-props/LineChartProps.ts
@@ -0,0 +1,27 @@
+export interface LineChartProps {
+  data: {
+    x: Date;
+    y: number;
+  }[];
+  paddingTop?: number;
+  paddingBottom?: number;
+  paddingLeft?: number;
+  paddingRight?: number;
+  width?: number;
+  height?: number;
+  lineColor?: string;
+  axesColor?: string;
+  addLabel?: boolean;
+  axesFontSize?: string;
+  xAxisLabel?: string;
+  yAxisLabel?: string;
+  axesLabelColor?: string;
+  axesLabelSize?: string;
+  animation?: boolean;
+  animationDuration?: number;
+  addTooltip?: boolean;
+  fontFamily?: string;
+  addTitle?: boolean;
+  setTitle?: string;
+  setTitleColor?: string;
+}
