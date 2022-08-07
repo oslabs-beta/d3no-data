80bb3f5b95ee8c01388569ac62bcc9177a9fc559
swittuthcoding <swittuthcoding@gmail.com>
2022-08-06 21:43:42 -0400

    moved export charts to another a separate file

---
+++ charts.ts
@@ -0,0 +1,6 @@
+import BarChart from "./chart-comps/BarChart.tsx";
+import LineChart from "./chart-comps/LineChart.tsx";
+import PieChart from "./chart-comps/PieChart.tsx";
+import ScatterPlotChart from "./chart-comps/ScatterPlotChart.tsx";
+
+export { BarChart, LineChart, PieChart, ScatterPlotChart };
